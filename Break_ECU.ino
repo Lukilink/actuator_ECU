f1ed56e4191ba126b5e3732d0b94041829bfcb4f
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2020-04-06 17:52:26 +0200

    Update and rename 05_break.ino to Break_ECU.ino

--- Break_ECU.ino
+++ Break_ECU.ino
@@ -1,4 +1,5 @@
-/* V5.0 Actuator ECU throttle
+/* V6.0 Actuator ECU throttle
+ * NEW VDO actuator without poti
 This sketach can be used to control a cruise control servo actuator over CAN. 
 */
 
@@ -7,20 +8,20 @@ This sketach can be used to control a cruise control servo actuator over CAN.
 
 
 //________________this values needs to be define for each car
-int PERM_ERROR = 1; // will allow a diffrence between targetPressure and currentPressure
+int PERM_ERROR = 4; // will allow a diffrence between targetPressure and currentPressure
 int minPot = 1; //measured at actuators lowest position
 int maxPot = 1000; //measured at actuators highest position
-int maxPressure = 100; // (250 when motor is on // 100 When motor is off is max of snesor) the max pressure your actuator is able to aply
-int minPressure = 85; //the pressure in stand still
-float maxACC_CMD = -477; //the max Value which comes from OP
-float minACC_CMD = -1500; //the min Value which comes from OP
+int maxPressure = 430; // the max pressure your actuator is able to aply
+int minPressure = 75; //the pressure in stand still
+float maxACC_CMD = 500; //the max Value which comes from OP
+float minACC_CMD = 0; //the min Value which comes from OP
 
 
 
 //________________define_pins
 int cancel_pin = 3;
-int potPin = A1;
 int pressurePin = A2;
+int breaklightPin = A5;
 
 int M_DIR = 8; // LOW is Left / HIGH is Right
 int M_PWM = 9; // 255 is run / LOW is stopp
@@ -28,10 +29,8 @@ int S_DIR = 7; // LOW is Left / HIGH is Right
 int S_PWM = 6; // 255 is run / LOW is stopp
 
 //________________values
-
 int targetPressure = 0;
 int currentPressure;
-int potiPosition = 0;
 
 float ACC_CMD_PERCENT = 0;
 float ACC_CMD = 0;
@@ -49,12 +48,12 @@ CAN.begin(500E3);
 
 //________________set up pin modes
 pinMode(cancel_pin, INPUT_PULLUP);
-pinMode(potPin, INPUT);    
 pinMode(pressurePin, INPUT);
 pinMode(M_DIR, OUTPUT);
 pinMode(M_PWM, OUTPUT);
 pinMode(S_DIR, OUTPUT);
 pinMode(S_PWM, OUTPUT);
+pinMode(breaklightPin, OUTPUT);
 digitalWrite(S_DIR, HIGH);
 
 }
@@ -63,11 +62,19 @@ void loop() {
 //________________read cancel pin
 cancel = true; //(digitalRead(cancel_pin)); 
 
-//________________read poti Position
-int potiPosition = (analogRead(potPin)); //read from pinA01
-
 //________________read pressure sensor
 currentPressure = (analogRead(pressurePin));
+
+//________________light up break lights
+if (currentPressure >= 75)
+  {
+   analogWrite(breaklightPin, 255);
+  }
+else 
+  {
+   analogWrite(breaklightPin, 0);
+  }
+Serial.println(currentPressure);
 
 //________________read ACC_CMD from CANbus
  CAN.parsePacket();
@@ -78,8 +85,9 @@ currentPressure = (analogRead(pressurePin));
       for (int ii = 0; ii <= 7; ii++) {
         dat[ii]  = (char) CAN.read();
         }
-        ACC_CMD = (dat[0] << 8 | dat[1] << 0); 
-       } 
+        ACC_CMD = ((dat[0] << 8 | dat[1] << 0) * -1); 
+        }    
+
 //________________calculating ACC_CMD into ACC_CMD_PERCENT
 if (ACC_CMD >= minACC_CMD) {
     ACC_CMD1 = ACC_CMD;
@@ -87,21 +95,14 @@ if (ACC_CMD >= minACC_CMD) {
 else {
     ACC_CMD1 = minACC_CMD;
     }
-
-
-ACC_CMD_PERCENT = 100; //((100/(maxACC_CMD - minACC_CMD)) * (ACC_CMD1 - minACC_CMD));
+       
+ACC_CMD_PERCENT = ((100/(maxACC_CMD - minACC_CMD)) * (ACC_CMD1 - minACC_CMD));
 
 //________________calculating tagetpressure from ACC_CMD_PERCENT
 float targetPressure = (((ACC_CMD_PERCENT / 100) * (maxPressure - minPressure)) + minPressure); // conversion from ACC_CMD_PERCENT % into targetpressure
   
 //________________do nothing if ACC_CMD is 0
 if (ACC_CMD_PERCENT == 0){
-   analogWrite(S_PWM, 0);  //open solenoid
-   analogWrite(M_PWM, 0);  //stop Motor
-}
-
-//________________do nothing if at endpoint
-if (potiPosition < minPot){
    analogWrite(S_PWM, 0);  //open solenoid
    analogWrite(M_PWM, 0);  //stop Motor
 }
@@ -114,21 +115,20 @@ while (!cancel) {
    }
 
 //________________close solenoid
-   analogWrite(S_PWM, 255);
+analogWrite(S_PWM, 255);
    
 //________________press or release the pedal to match targetPressure & respect endpoints
-
 if (abs(currentPressure - targetPressure) >= PERM_ERROR)
   {
-    if ((currentPressure < targetPressure) && (potiPosition < maxPot))
+    if (currentPressure < targetPressure)
         { 
         analogWrite(M_PWM, 255);  //run Motor
-        digitalWrite(M_DIR, LOW); //motor driection left
+        digitalWrite(M_DIR, HIGH); //motor driection left
         }    
-    else if ((currentPressure > targetPressure) && (potiPosition >= minPot))
+    else if (currentPressure > targetPressure)
         {       
         analogWrite(M_PWM, 255);   //run Motor
-        digitalWrite(M_DIR, HIGH); //motor driection right
+        digitalWrite(M_DIR, LOW); //motor driection right
         }
   }  
   
@@ -148,12 +148,8 @@ else {
 //Serial.print("target_");
 //Serial.print(targetPressure);
 //Serial.print("_____");
-Serial.print("Position_");
-Serial.print(potiPosition);
-Serial.print("_____");
-Serial.print("Pressure");
-Serial.print(currentPressure);
-Serial.println("");
-
+//Serial.print("Pressure");
+//Serial.println(currentPressure);
+//Serial.println("");
 
 }
